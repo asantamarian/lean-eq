d102e96bfed583a89bea031bd626981f2a9260c3
andresfsantamarian <asantnor@gmail.com>
2023-04-25 19:27:50 -0400

    added paramaeters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -55,6 +55,9 @@ public:
     //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    static juce::AudioProcessorValueTreeState::ParameterLayout  createParameterLayOut();
+    juce::AudioProcessorValueTreeState apvts{*this,nullptr,"Parameters", createParameterLayOut()};
 
 private:
     //==============================================================================
